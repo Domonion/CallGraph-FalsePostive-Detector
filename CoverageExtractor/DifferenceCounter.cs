7ea9876bc3660d4449fd5d987c426dce85e5e4af
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2019-12-16 05:06:25 +0300

    added hashset for every stats property to unique input

--- CoverageExtractor/DifferenceCounter.cs
+++ CoverageExtractor/DifferenceCounter.cs
@@ -8,18 +8,31 @@ namespace CoverageExtractor
     {
         private readonly HashSet<string> myExpectedUsed;
         private readonly HashSet<string> myExpectedUnused;
-        private int myOkey;
-        private int myTotal;
-        private int myExpectedTotal;
-        private int myFalsePositive;
-        private int myFalseNegative;
-        private int myUnknown;
+        private readonly HashSet<string> myFalsePositive;
+        private readonly HashSet<string> myUnknown;
+        private readonly HashSet<string> myOkey;
+        private readonly HashSet<string> myFalseNegative;
+        private readonly HashSet<string> myTotal;
+        private readonly HashSet<string> myExpectedTotal;
 
         public DifferenceCounter()
         {
-            myExpectedUnused = new HashSet<string>();
-            myOkey = myTotal = myExpectedTotal = myFalseNegative = myFalsePositive = myUnknown = 0;
             myExpectedUsed = new HashSet<string>();
+            myExpectedUnused = new HashSet<string>();
+            myFalsePositive = new HashSet<string>();
+            myUnknown = new HashSet<string>();
+            myOkey = new HashSet<string>();
+            myFalseNegative = new HashSet<string>();
+            myTotal = new HashSet<string>();
+            myExpectedTotal = new HashSet<string>();
+        }
+
+        private static void Dump(TextWriter writer, IEnumerable<string> set)
+        {
+            foreach (var str in set)
+            {
+                writer.WriteLine(str);
+            }
         }
 
         public void CalculateError(string expectedUsed, string expectedUnused, string used, string unused,
@@ -33,25 +46,31 @@ namespace CoverageExtractor
             AssertExistence(expectedUnused);
             AssertExistence(unused);
             AssertExistence(used);
-            
+
             AssertExtension(expectedUnused, ".gold");
             AssertExtension(expectedUsed, ".gold");
             AssertExtension(used, ".temp");
             AssertExtension(unused, ".temp");
-            
+
             FillSet(myExpectedUsed, expectedUsed);
             FillSet(myExpectedUnused, expectedUnused);
-            using (StreamWriter errorsWriter = File.CreateText(errors), statisticsWriter = File.CreateText(statistics),
-                    unkownWriter = File.CreateText(unknown))
+            /*
+             * если используемость в обоих фреймворках сопадает - то тогда ответ на этот метод совпадает в обоих случаях
+             * если используемость разнится - то из ковра он попадет в оба множества. тогда вне зависиомсти от варнинга метод попадёт в ок.
+             * потому я видимо могу забить на разные фреймворки, единственное - мне нужно сохранять фп, чтобы они не повторялись на фреймворках!
+             */
+            using (StreamWriter errorsWriter = File.CreateText(errors),
+                statisticsWriter = File.CreateText(statistics),
+                unknownWriter = File.CreateText(unknown))
             {
-                CheckContent(true, used, errorsWriter, unkownWriter);
-                CheckContent(false, unused, errorsWriter, unkownWriter);
-                statisticsWriter.WriteLine("Expected total: " + myExpectedTotal);
-                statisticsWriter.WriteLine("Input total: " + myTotal);
-                statisticsWriter.WriteLine("Ok: " + myOkey);
-                statisticsWriter.WriteLine("False positives: " + myFalsePositive);
-                statisticsWriter.WriteLine("False negatives: " + myFalseNegative);
-                statisticsWriter.WriteLine("Unknown: " + myUnknown);
+                CheckContent(true, used);
+                CheckContent(false, unused);
+                statisticsWriter.WriteLine("Gold total: " + myExpectedTotal.Count);
+                statisticsWriter.WriteLine("Input total: " + myTotal.Count);
+                statisticsWriter.WriteLine("Ok: " + myOkey.Count);
+                statisticsWriter.WriteLine("False positives: " + myFalsePositive.Count);
+                statisticsWriter.WriteLine("False negatives: " + myFalseNegative.Count);
+                statisticsWriter.WriteLine("Unknown: " + myUnknown.Count);
                 var otstup = new string(' ', 10);
                 statisticsWriter.WriteLine("Gold data taken from:");
                 statisticsWriter.WriteLine(otstup + expectedUsed);
@@ -59,42 +78,42 @@ namespace CoverageExtractor
                 statisticsWriter.WriteLine("Test data taken from:");
                 statisticsWriter.WriteLine(otstup + used);
                 statisticsWriter.WriteLine(otstup + unused);
+                Dump(errorsWriter, myFalsePositive);
+                Dump(unknownWriter, myUnknown);
                 statisticsWriter.WriteLine("False positives listed in: " + errors);
                 statisticsWriter.WriteLine("Uknowns listed in: " + unknown);
             }
         }
 
-        private void Check(string input, bool used, TextWriter writer, TextWriter unknown)
+        private void Check(string input, bool used)
         {
             if (used && myExpectedUsed.Contains(input) || !used && myExpectedUnused.Contains(input))
             {
-                myOkey++;
+                myOkey.Add(input);
             }
             else if (used && myExpectedUnused.Contains(input))
             {
-                myFalseNegative++;
+                myFalseNegative.Add(input);
             }
-            else if(!used && myExpectedUsed.Contains(input))
+            else if (!used && myExpectedUsed.Contains(input))
             {
-                myFalsePositive++;
-                writer.WriteLine(input);
+                myFalsePositive.Add(input);
             }
             else
             {
-                myUnknown++;
-                unknown.WriteLine(input);
+                myUnknown.Add(input);
             }
         }
 
-        private void CheckContent(bool used, string path, StreamWriter errors, StreamWriter unknown)
+        private void CheckContent(bool used, string path)
         {
             using (var reader = File.OpenText(path))
             {
                 string str;
                 while ((str = reader.ReadLine()) != null)
                 {
-                    myTotal++;
-                    Check(str, used, errors, unknown);
+                    myTotal.Add(str);
+                    Check(str, used);
                 }
             }
         }
@@ -106,7 +125,7 @@ namespace CoverageExtractor
                 string str;
                 while ((str = streamReader.ReadLine()) != null)
                 {
-                    myExpectedTotal++;
+                    myExpectedTotal.Add(str);
                     set.Add(str);
                 }
             }
